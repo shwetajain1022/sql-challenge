b079c34503e2e34ba35d59e2dca5a9676fbc1988
shwetajain1022 <40103518+shwetajain1022@users.noreply.github.com>
2023-02-16 10:54:29 +0800

    updated EmployeeSQL

--- sql-challenge/EmployeeSQL/EmployeesSchemaCreation.sql
+++ sql-challenge/EmployeeSQL/EmployeesSchemaCreation.sql
@@ -1,18 +1,18 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/rLdP7i
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/Cjleuo
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
 CREATE TABLE "titles" (
-    "title_id" varchar(10)   NOT NULL,
-    "title" varchar(50)   NOT NULL,
+    "title_id" varchar(6)   NOT NULL,
+    "title" varchar(200)   NOT NULL,
     CONSTRAINT "pk_titles" PRIMARY KEY (
         "title_id"
      )
 );
 
 CREATE TABLE "departments" (
-    "dept_code" varchar(10)   NOT NULL,
+    "dept_code" varchar(5)   NOT NULL,
     "dept_name" varchar(100)   NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_code"
@@ -20,57 +20,57 @@ CREATE TABLE "departments" (
 );
 
 CREATE TABLE "Employee" (
-    "emp_id" int   NOT NULL,
-    "emp_title_id" varchar(10)   NOT NULL,
+    "emp_no" int   NOT NULL,
+    "emp_title_id" varchar(6)   NOT NULL,
     "birth_date" Date   NOT NULL,
     "first_name" varchar(100)   NULL,
     "last_name" varchar(100)   NULL,
-    "sex" varchar(1) NULL,
+    "sex" varchar(1)   NOT NULL,
     "hire_date" Date   NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
-        "emp_id"
+    CONSTRAINT "pk_Employee" PRIMARY KEY (
+        "emp_no"
      )
 );
 
 CREATE TABLE "dept_emp" (
-    "dept_code" varchar(10)   NOT NULL,
-    "emp_id" INT   NOT NULL,
+    "dept_code" varchar(5)   NOT NULL,
+    "emp_no" INT   NOT NULL,
     CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "dept_code","emp_id"
+        "dept_code","emp_no"
      )
 );
 
 CREATE TABLE "salaries" (
-    "emp_id" int   NOT NULL,
+    "emp_no" int   NOT NULL,
     "salary" float   NULL,
     CONSTRAINT "pk_salaries" PRIMARY KEY (
-        "emp_id"
+        "emp_no"
      )
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_code" varchar(10)   NOT NULL,
-    "emp_id" INT   NOT NULL,
+    "dept_code" varchar(5)   NOT NULL,
+    "emp_no" INT   NOT NULL,
     CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "dept_code","emp_id"
+        "dept_code","emp_no"
      )
 );
 
-ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+ALTER TABLE "Employee" ADD CONSTRAINT "fk_Employee_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "titles" ("title_id");
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_code" FOREIGN KEY("dept_code")
 REFERENCES "departments" ("dept_code");
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_id" FOREIGN KEY("emp_id")
-REFERENCES "Employees" ("emp_id");
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employee" ("emp_no");
 
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_id" FOREIGN KEY("emp_id")
-REFERENCES "Employees" ("emp_id");
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employee" ("emp_no");
 
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_code" FOREIGN KEY("dept_code")
 REFERENCES "departments" ("dept_code");
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_id" FOREIGN KEY("emp_id")
-REFERENCES "Employees" ("emp_id");
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employee" ("emp_no");
 
